447540d2f9088815bdb7c94e55c3fc83dc709f12
Igor <bird-starling@ya.ru>
2022-01-16 11:46:18 +0300

    in progress

---
+++ src/main/java/org/example/AboutController.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class AboutController {
+
+    @FXML
+    private void closeAbout() throws IOException {
+        System.exit(2);
+    }
+}
